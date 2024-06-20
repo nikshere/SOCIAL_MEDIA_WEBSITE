20111083d57dbe2a5b17f288a3e7671a6561de02
Navneet singh <133386944+nikshere@users.noreply.github.com>
2024-06-20 23:01:04 +0530

    Update dashboard.js

--- client/pages/user/dashboard.js
+++ client/pages/user/dashboard.js
@@ -14,6 +14,7 @@ import { useRouter } from "next/router";
 import ChatButton from "../../components/ChatButton";
 const Avatar = dynamic(() => import("antd/lib/avatar"));
 const Modal = dynamic(() => import("antd/lib/modal"), { ssr: false });
+import Comment from "../../components/showComments";
 import CommentForm from "../../components/forms/ComentForm"
 
 const socket = io(process.env.NEXT_PUBLIC_SOCKETIO, {
